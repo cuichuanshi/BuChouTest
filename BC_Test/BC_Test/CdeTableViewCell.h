fc6e662497a5201bfb525ab85a26a4d99dfd333d
崔传世 <juncheng@xiaotimo.local>
2018-04-09 10:31:37 +0800

    新增

---
+++ BC_Test/BC_Test/CdeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CdeTableViewCell.h
+//  BC_Test
+//
+//  Created by juncheng on 2018/4/9.
+//  Copyright © 2018年 buchouwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CdeTableViewCell : UITableViewCell
+
+@end
---
+++ BC_Test/BC_Test/CdeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CdeTableViewCell.m
+//  BC_Test
+//
+//  Created by juncheng on 2018/4/9.
+//  Copyright © 2018年 buchouwang. All rights reserved.
+//
+
+#import "CdeTableViewCell.h"
+
+@implementation CdeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
