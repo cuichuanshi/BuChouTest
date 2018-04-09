49b4fbf777de16d6f35ede8d346cb63b3d03abfc
崔传世 <juncheng@xiaotimo.local>
2018-04-09 09:49:55 +0800

    初建项目

---
+++ BC_Test/BC_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BC_Test
+//
+//  Created by juncheng on 2018/4/9.
+//  Copyright © 2018年 buchouwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BC_Test/BC_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BC_Test
+//
+//  Created by juncheng on 2018/4/9.
+//  Copyright © 2018年 buchouwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
